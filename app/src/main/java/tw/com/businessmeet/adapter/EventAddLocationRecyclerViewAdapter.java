aefe686f84dca17551261e72be0e09110b9575d4
Alice Lee <alice880405@gmail.com>
2020-09-27 16:30:10 +0800

    create event add location.participant adapter

--- app/src/main/java/tw/com/businessmeet/adapter/EventAddLocationRecyclerViewAdapter.java
+++ app/src/main/java/tw/com/businessmeet/adapter/EventAddLocationRecyclerViewAdapter.java
@@ -1,4 +1,49 @@
 package tw.com.businessmeet.adapter;
 
-public class EventAddLocationRecyclerViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import tw.com.businessmeet.R;
+
+public class EventAddLocationRecyclerViewAdapter extends RecyclerView.Adapter<EventAddLocationRecyclerViewAdapter.ViewHolder> {
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public EventAddLocationRecyclerViewAdapter(Context context) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+
+    }
+
+    @NonNull
+    @Override
+    public EventAddLocationRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.recycler_view_row_event_add_location, parent,false);
+        return new EventAddLocationRecyclerViewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public interface ClickListener {
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
